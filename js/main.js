29d845751653e75f0ab2ce52001331537b8ca167
drozdovigor1989 <63999148+drozdovigor1989@users.noreply.github.com>
2021-02-15 18:45:51 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,54 @@
+$(function () {
+
+    $('.top__slider').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        // fade:true,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings:{
+                dots: false,
+                
+            }
+        }
+        ]
+
+    });
+$('.reviews__slider').slick({
+    arrows: false,
+    dots: true,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1100,
+            settings:{
+                slidesToShow: 3,
+            }     
+        },
+        {
+            breakpoint: 841,
+            settings:{
+                slidesToShow: 2,
+            
+        }
+    },
+    {
+        breakpoint: 550,
+        settings:{
+            slidesToShow: 1,
+        
+    }
+},
+    ]
+
+});
+
+$('.menu__btn').on('click', function () {
+    $('.menu__list').toggleClass('menu__list--active')
+})
+
+});
